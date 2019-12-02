fda74e1f93362c1ecf958fe8124454f1f22dc67a
Jennie <cassiopeia5571@gmail.com>
2019-12-02 02:10:08 -0600

    uncommented code in accountdataservice file

--- src/api/AccountDataService.js
+++ src/api/AccountDataService.js
@@ -1,26 +1,26 @@
-// import axios from "axios";
-// import { JPA_API_URL } from "../Constants"
+import axios from "axios";
+import { JPA_API_URL } from "../Constants"
 
-// class AccountDataService {
-//     retrieveAllAccounts(name) {
-//         return axios.get(`${JPA_API_URL}/users/${name}/accounts`);
-//     }
+class AccountDataService {
+    retrieveAllAccounts(name) {
+        return axios.get(`${JPA_API_URL}/users/${name}/accounts`);
+    }
 
-//     retrieveAccount(name, accountId) {
-//         return axios.get(`${JPA_API_URL}/users/${name}/accounts/${accountId}`);
-//     }
+    retrieveAccount(name, accountId) {
+        return axios.get(`${JPA_API_URL}/users/${name}/accounts/${accountId}`);
+    }
 
-//     deleteAccount(name, accountId) {
-//         return axios.delete(`${JPA_API_URL}/users/${name}/accounts/${accountId}`);
-//     }
+    deleteAccount(name, accountId) {
+        return axios.delete(`${JPA_API_URL}/users/${name}/accounts/${accountId}`);
+    }
 
-//     updateAccount(name, id, account) {
-//         return axios.put(`${JPA_API_URL}/users/${name}/accounts/${id}`, account);
-//     }
+    updateAccount(name, id, account) {
+        return axios.put(`${JPA_API_URL}/users/${name}/accounts/${id}`, account);
+    }
 
-//     createAccount(name, account) {
-//         return axios.post(`${JPA_API_URL}/users/${name}/accounts/`, account);
-//     }
-// }
+    createAccount(name, account) {
+        return axios.post(`${JPA_API_URL}/users/${name}/accounts/`, account);
+    }
+}
 
-// export default new AccountDataService();
+export default new AccountDataService();
