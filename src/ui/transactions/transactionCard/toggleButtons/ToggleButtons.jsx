2ee7b401eca2b8d02582efdd36610b46a79d77dd
Jennie <cassiopeia5571@gmail.com>
2020-01-22 16:04:25 -0600

    toggle button updated

--- src/ui/transactions/transactionCard/toggleButtons/ToggleButtons.jsx
+++ src/ui/transactions/transactionCard/toggleButtons/ToggleButtons.jsx
@@ -6,17 +6,23 @@ import "./ToggleButtons.css";
 
 class ToggleButtons extends Component {
 
+  thirtyDays() {
+    console.log("30 days of transactions")
+    // event.Date.today()
+    //   .add(-30)
+    //   .days();
+  }
 
   render() {
     return (
       <div className="btn-group btn-group-lg" role="group">
-        <button type="button" className="btn" id="toggle1" >
+        <button type="button" className="btn" id="current" onClick= {this.thirtyDays()}>
           Current
         </button>
-        <button type="button" className="btn" id="toggle2">
+        <button type="button" className="btn" id="all">
           All
         </button>
-      </div>      
+      </div>
     );
   }
 }
