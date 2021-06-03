5feed712c857706859a683c9d8b5365718b9512e
Cassiopeia <cassiopeia5571@gmail.com>
2021-06-03 17:10:36 -0500

    Update Account.java

--- manage-my-finances_backend/src/main/java/com/jennieCreation/managemyfinances_backend/account/Account.java
+++ manage-my-finances_backend/src/main/java/com/jennieCreation/managemyfinances_backend/account/Account.java
@@ -8,100 +8,85 @@ import java.util.Date;
 
 @Entity
 public class Account {
-	
-	@Id
-	@GeneratedValue
-	private long id;
-	private String username;
-	private String accountName;
-	private double deposits;
-	private double withdrawals;
-	private Date asOfDate;
-	
-	@OneToMany(mappedBy="account", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    	private List<Transaction> transactions = new ArrayList<>();
+    
+    @Id
+    @GeneratedValue
+    public long id;
+    private String username;
+    private String accountName;
+    private Date asOfDate;
+    
+    @OneToMany(mappedBy="account", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Transaction> transactions = new ArrayList<>();
 
-	protected Account() {
-		
-	}
+    protected Account() {
+        
+    }
+    
+    public Account(String username, String accountName, Date asOfDate) {
+        super();
+        this.username = username;
+        this.accountName = accountName;
+        this.asOfDate = asOfDate;
+    }
 
-	public Account(long id, String username, String accountName, double deposits, double withdrawals, Date asOfDate) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.accountName = accountName;
-		this.deposits = deposits;
-		this.withdrawals = withdrawals;
-		this.asOfDate = asOfDate;
-	}
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
 
-	public String getAccountName() {
-		return accountName;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setAccountName(String accountName) {
-		this.accountName = accountName;
-	}
 
-	public double getDeposits() {
-		return deposits;
-	}
+    public String getAccountName() {
+        return accountName;
+    }
 
-	public void setDeposits(double deposits) {
-		this.deposits = deposits;
-	}
 
-	public double getWithdrawals() {
-		return withdrawals;
-	}
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
 
-	public void setWithdrawals(double withdrawals) {
-		this.withdrawals = withdrawals;
-	}
 
-	public Date getAsOfDate() {
-		return asOfDate;
-	}
+    public Date getAsOfDate() {
+        return asOfDate;
+    }
 
-	public void setAsOfDate(Date asOfDate) {
-		this.asOfDate = asOfDate;
-	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
+    public void setAsOfDate(Date asOfDate) {
+        this.asOfDate = asOfDate;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Account other = (Account) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Account other = (Account) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
 }
