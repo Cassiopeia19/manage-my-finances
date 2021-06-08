53eb678c1d44e1445eaecae53c92ef12eb46f8d1
Cassiopeia <cassiopeia5571@gmail.com>
2021-06-08 15:18:36 -0500

    Update TransactionJpaResource.java

--- manage-my-finances_backend/src/main/java/com/jennieCreation/managemyfinances_backend/transaction/TransactionJpaResource.java
+++ manage-my-finances_backend/src/main/java/com/jennieCreation/managemyfinances_backend/transaction/TransactionJpaResource.java
@@ -2,6 +2,7 @@ package com.jennieCreation.managemyfinances_backend.transaction;
 
 import java.net.URI;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,54 +21,45 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class TransactionJpaResource {
 
 	@Autowired
-	private TransactionService transactionService;
-	
-	@Autowired
 	private TransactionJpaRepository transactionJpaRepository;
 
 	@GetMapping("/jpa/users/{username}/transactions")
 	public List<Transaction> getAllTransactions(@PathVariable String username) {
 		return transactionJpaRepository.findByUsername(username);
 	}
-	
+
 	@GetMapping("/jpa/users/{username}/transactions/{id}")
 	public Transaction getTransaction(@PathVariable String username, @PathVariable long id) {
 		return transactionJpaRepository.findById(id).get();
 	}
 
-	// DELETE /users/{username}/transactions/{id}
 	@DeleteMapping("/jpa/users/{username}/transactions/{id}")
 	public ResponseEntity<Void> deleteTransaction(@PathVariable String username, @PathVariable long id) {
-
 		transactionJpaRepository.deleteById(id);
-
-			return ResponseEntity.noContent().build();
+		return ResponseEntity.noContent().build();
 	}
-	
-	//Edit/Update a Transaction
-		//PUT /users/{username}/transactions/{id}
+
 	@PutMapping("/jpa/users/{username}/transactions/{id}")
-	public ResponseEntity<Transaction> updateTransaction(
-			@PathVariable String username,
-			@PathVariable long id, @RequestBody Transaction transaction){
-		
+	public ResponseEntity<Transaction> updateTransaction(@PathVariable String username, @PathVariable long id,
+			@RequestBody Transaction transaction) {
 		transaction.setUsername(username);
-		
 		Transaction transactionUpdated = transactionJpaRepository.save(transaction);
-		
 		return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
 	}
-			
+
 	@PostMapping("/jpa/users/{username}/transactions")
-	public ResponseEntity<Void> createTransaction (
-			@PathVariable String username, @RequestBody Transaction transaction){
-		
-		transaction.setUsername(username);
-		
+	public ResponseEntity<String> createTransaction(@PathVariable String username,
+			@RequestBody Transaction transaction) {
+		transaction.setUsername(username);		
 		Transaction createdTransaction = transactionJpaRepository.save(transaction);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(createdTransaction.getId()).toUri();
-		
-		return ResponseEntity.created(uri).build();
-	}	
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(createdTransaction.getId()).toUri();
+		ResponseEntity<Object> build = ResponseEntity.created(uri).build();
+		int statusCodeValue = build.getStatusCodeValue();
+		if (build.getStatusCodeValue() == 201) {
+			return new ResponseEntity<String>("Data has been Inserted Successfully", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
